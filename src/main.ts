54112e9a2a4486eb6ce54dd4ece91d2521b14fec
Samuel Ballan <sgb4622@gmail.com>
2019-04-09 18:16:04 -0400

    add bulma

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// include bulma css library
+import './../node_modules/bulma/css/bulma.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
