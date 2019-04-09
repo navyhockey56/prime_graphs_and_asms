7857c5b0b2e29e344fe4027ac072d00faab30f3c
Samuel Ballan <sgb4622@gmail.com>
2019-04-09 18:30:58 -0400

    controls

---
+++ src/components/Controls.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="field is-grouped">
+    <p class="control">
+      <label class="label">Activators</label>
+      <input class="input is-rounded is-small" type="text" value="1">
+    </p>
+    <p class="control">
+      <label class="label">Non-Activators</label>
+      <input class="input is-rounded is-small" type="text" value="1">
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
-  <Cycle type="square" :length="5" :activeIndex="3"/>
+  <div>
+    <h1 class="title">Welcome to Prime Graphs and ASMs</h1>
+    <Controls/>
+    <Cycle type="square" :length="5" :activeIndex="3"/>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 import Cycle from '@/components/Cycle.vue';
+import Controls from '@/components/Controls.vue';
 
 export default Vue.extend({
   name: 'home',
   components: {
     HelloWorld,
     Cycle,
+    Controls
   },
 });
 </script>
