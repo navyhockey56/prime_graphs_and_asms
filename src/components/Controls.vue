2dff17eb38fff9df6bfbfcc8906dd4a67ca34d71
Samuel Ballan <sgb4622@gmail.com>
2019-04-09 20:52:04 -0400

    connect controls

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,19 +2,42 @@
   <div class="field is-grouped">
     <p class="control">
       <label class="label">Activators</label>
-      <input class="input is-rounded is-small" type="text" value="1">
+      <input v-model="activators" @input="updateActivators" class="input is-rounded is-small" type="text">
     </p>
     <p class="control">
       <label class="label">Non-Activators</label>
-      <input class="input is-rounded is-small" type="text" value="1">
+      <input v-model="nonActivators" @input="updateNonActivators" class="input is-rounded is-small" type="text" value="1">
+    </p>
+    <p class="control">
+      <button class="button">Next</button>
     </p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
+import { mapActions, mapState } from 'vuex'
 
+export default Vue.extend({
+  data() {
+    return {
+      activators: this.$store.state.activators,
+      nonActivators: this.$store.state.nonActivators
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setActivators',
+      'setNonActivators'
+    ]),
+    updateActivators(e: Event) {
+      console.log('test')
+      this.setActivators(e.target && e.target.value)
+    },
+    updateNonActivators(e: Event) {
+      this.setNonActivators(e.target && e.target.value)
+    }
+  },
 })
 </script>
 
--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,23 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    activators: 1,
+    nonActivators: 1
   },
   mutations: {
-
+    setActivators(state, num: number) {
+      state.activators = num;
+    },
+    setNonActivators(state, num: number) {
+      state.nonActivators = num;
+    },
   },
   actions: {
-
+    setActivators(context, num: number) {
+      context.commit('setActivators', num);
+    },
+    setNonActivators(context, num: number) {
+      context.commit('setNonActivators', num);
+    },
   },
 });
